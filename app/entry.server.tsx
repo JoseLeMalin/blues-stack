f2570191a9ec373826437f69403a623807ab0522
JoseLeMalin <seb_488@hotmail.fr>
2024-10-07 22:30:03 +0200

    init graphQL

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -3,7 +3,8 @@
  * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
  * For more information, see https://remix.run/docs/en/main/file-conventions/entry.server
  */
-
+// https://www.apollographql.com/docs/graphos/reference/guides/graphql-and-rest-together
+// https://www.apollographql.com/docs/apollo-server/data/fetching-rest
 import { PassThrough } from "node:stream";
 
 import type { EntryContext } from "@remix-run/node";
@@ -11,6 +12,15 @@ import { createReadableStreamFromReadable } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
 import { isbot } from "isbot";
 import { renderToPipeableStream } from "react-dom/server";
+import { renderToString } from "react-dom/server";
+import { ApolloProvider } from "@apollo/client/react";
+import {
+  
+  ApolloClient,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from "@apollo/client/core";
+import { HttpLink } from "@apollo/client/link/http";
 
 const ABORT_DELAY = 5_000;
 
@@ -20,18 +30,47 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
+  const apolloClient = new ApolloClient({
+    ssrMode: true,
+    cache: new InMemoryCache(),
+    link: new HttpLink({
+      uri: "https://rickandmortyapi.com/graphql",
+      headers: {
+        "Access-Control-Allow-Origin": "*", //Cors management
+      },
+      credentials: request.credentials ?? "include", // or "same-origin" if your backend server is the same domain
+    }),
+  });
+
+  /* const App = (
+    <ApolloProvider client={apolloClient}>
+      <RemixServer context={remixContext} url={request.url} />
+    </ApolloProvider>
+  ); */
+
+  // TODO: update everything below this line
+  /* const markup = renderToString(App); */
+
+  /*  responseHeaders.set("Content-Type", "text/html"); */
+
+  /* return new Response("<!DOCTYPE html>" + markup, {
+    status: responseStatusCode,
+    headers: responseHeaders,
+  }); */
   return isbot(request.headers.get("user-agent"))
     ? handleBotRequest(
         request,
         responseStatusCode,
         responseHeaders,
         remixContext,
+        apolloClient,
       )
     : handleBrowserRequest(
         request,
         responseStatusCode,
         responseHeaders,
         remixContext,
+        apolloClient,
       );
 }
 
@@ -40,14 +79,17 @@ function handleBotRequest(
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext,
+  apolloClient: ApolloClient<NormalizedCacheObject>,
 ) {
   return new Promise((resolve, reject) => {
     const { abort, pipe } = renderToPipeableStream(
-      <RemixServer
-        context={remixContext}
-        url={request.url}
-        abortDelay={ABORT_DELAY}
-      />,
+      <ApolloProvider client={apolloClient}>
+        <RemixServer
+          context={remixContext}
+          url={request.url}
+          abortDelay={ABORT_DELAY}
+        />
+      </ApolloProvider>,
       {
         onAllReady() {
           const body = new PassThrough();
@@ -82,14 +124,17 @@ function handleBrowserRequest(
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext,
+  apolloClient: ApolloClient<NormalizedCacheObject>,
 ) {
   return new Promise((resolve, reject) => {
     const { abort, pipe } = renderToPipeableStream(
-      <RemixServer
-        context={remixContext}
-        url={request.url}
-        abortDelay={ABORT_DELAY}
-      />,
+      <ApolloProvider client={apolloClient}>
+        <RemixServer
+          context={remixContext}
+          url={request.url}
+          abortDelay={ABORT_DELAY}
+        />
+      </ApolloProvider>,
       {
         onShellReady() {
           const body = new PassThrough();
